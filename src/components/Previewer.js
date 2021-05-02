e3cf2a316acb861caf81ac28e7d647884bb54009
marcocosta1618 <marcocosta1618@gmail.com>
2021-05-02 11:44:20 +0200

    Add marked options: all fcc tests passing.
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,35 +1,39 @@
 import React, { Component } from 'react';
-import initialize from  './initState';
+import initState from './initialize';
 
 const marked = require('marked');
-
+marked.setOptions({
+    gfm: true,
+    breaks: true,
+    headerIds: false
+})
 
 export class Editor extends Component {
-      constructor(props) {
-          super(props);
-          this.state = {
-              input: initialize
-          }
-          this.handleChange = (event) => {
-              this.setState({
-                  input: event.target.value
-              })
-          }
-      }
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: initState
+        }
+        this.handleChange = (event) => {
+            this.setState({
+                input: event.target.value
+            })
+        }
+    }
 
 
-      render() {
-          const markUp = () => {
-              return {__html: marked(this.state.input)};
-          }
-          
-          return (
-              <>
-              <textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
-              <Preview markdown={markUp()}></Preview>
-              </>
-          )
-      }
+    render() {
+        const markUp = () => {
+            return { __html: marked(this.state.input) };
+        }
+
+        return (
+            <div>
+                <textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
+                <Preview markdown={markUp()}></Preview>
+            </div>
+        )
+    }
 }
 
 export const Preview = (props) => {
--- src/components/initialize.js
+++ src/components/initialize.js
@@ -1,23 +1,37 @@
 // initialize the state of the Editor component with a default markdown
 
-const initialize = 
+const initState = 
 `# Hello 👋👋👋,
 ## and welcome to my little markdown previewer.
-It uses [marked](https://marked.js.org/) to convert your markdown into HTML...how cool is that??  
+It uses [\`marked\`](https://marked.js.org/) to convert your markdown into HTML...how cool is that??  
 
 Now, we can easily do a bunch of stuff with this tool. Let's make a list:
 
 - this is an item in an unordered list;
 - this is *another one*, in italicized text;
-- of course we can have **bold items**;
+- we can have **bold items**, of course;
 - and, just because we can, an ordered list within an unordered list:
     1. Lorem;
     2. Ipsum;
     3. Dolor;
-    
+   
+
+A tasks list:
+
+- [x] something done
+- [ ] something to do
+- [ ] something else to do
+
+And a table:
+
+| Tables  |      Are      |  Cool   | 
+|   ---   |      ---      |   ---   |
+|  item1  |     item2     |  item3  |
+|  item4  |     item5     |  item6  |    
+
 ### But wait, there's more!
 
-We can display \`code\`, and also code box, like so:
+We can display inline code, as in \`const square = (x) => x * x;\`  and also code box, like so:
 ~~~
 function App() {
     return (
@@ -36,9 +50,9 @@ Want to quote your favorite author? We got you covered!
 >
 > *Isaac Asimov* 
 
-![A beautiful night sky from a lake surrounded by snowy mountains.](https://i.postimg.cc/nz8hdWgz/night-sky-2.jpg)
+![A robot laying down on the side waving hello.](https://i.postimg.cc/z3mdq9BM/robot-01.jpg)
 
 `
 
-export default initialize;
+export default initState;
 
